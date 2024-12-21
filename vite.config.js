303d20c985405d233a6113bae7f2896155bffb4e
Ivan Rodriguez <Irodriguez@DESKTOP-J1JP2L6>
2024-12-21 16:05:24 -0500

    h

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/nombre-repo/', // Cambia '/nombre-repo/' por el nombre de tu repositorio o subdirectorio
   plugins: [react()],
   build: {
-    outDir: 'dist', // Asegúrate de que esta es la carpeta de salida
-  }
+    outDir: 'dist',  // La carpeta de salida
+    rollupOptions: {
+      input: './src/main.jsx',  // Asegúrate de que el archivo está correctamente configurado
+    },
+  },
 })
+
 
 
 //import { defineConfig } from 'vite'
