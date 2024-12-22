9d4e1946146dd4d56199926aa4e7d4ad1199ebe8
Ivan Rodriguez <Irodriguez@DESKTOP-J1JP2L6>
2024-12-21 23:00:21 -0500

    Configuración para deploy en GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/nombre-repo/', // Cambia '/nombre-repo/' por el nombre de tu repositorio o subdirectorio
   plugins: [react()],
   build: {
-    outDir: 'dist',  // La carpeta de salida
-    rollupOptions: {
-      input: './src/main.jsx',  // Asegúrate de que el archivo está correctamente configurado
-    },
-  },
+    outDir: 'dist', // Asegúrate de que esta es la carpeta de salida
+  }
 })
-
 
 
 //import { defineConfig } from 'vite'
