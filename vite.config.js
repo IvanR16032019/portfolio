b792dae8d6d8df148fc4f74c3ba5932a0978250a
Ivan Rodriguez <Irodriguez@DESKTOP-J1JP2L6>
2024-12-22 11:04:15 -0500

    b

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Cambiar la raíz del proyecto a la carpeta 'src'
+  root: 'src',
+  build: {
+    outDir: '../dist', // Salida en la carpeta 'dist', que debe estar fuera de 'src'
+  },
+  publicDir: '../public', // Si tienes archivos estáticos como imágenes, ponlos en 'public'
+});
+
+
+
 
 
 //import { defineConfig } from 'vite'
