8d86657541c75768db34832b98afc0375d55addb
Ivan Rodriguez <Irodriguez@DESKTOP-J1JP2L6>
2024-12-22 09:11:29 -0500

    kk

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // Reemplaza con el nombre de tu repositorio
 })
+
 
 
 //import { defineConfig } from 'vite'
