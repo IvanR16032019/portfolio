f770c2c1711144c9c128ceb9bbe86bf704e3b444
Ivan Rodriguez <Irodriguez@DESKTOP-J1JP2L6>
2024-12-22 11:37:24 -0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',  // Los archivos construidos se almacenarán en la carpeta 'dist'
-  },
+  base: "/portfolio"
 });
 
 
